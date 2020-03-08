e8664706f636be387044dade2b26d3e3067f7628
Kane Punter <brit.in.stuttgart@gmail.com>
2020-03-08 23:10:03 +0100

    updated git ignore

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'AIzaSyDBvu5D94WBkPYj3mFD9pl8rwwIIM_8ulQ'
 };
 
 /*
